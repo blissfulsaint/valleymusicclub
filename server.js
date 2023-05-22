dacfd8703602f27e8fdab9e0d393426864259dab
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-05-22 15:41:27 -0600

    cors

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app
     .use(cors())
     .use(express.json())
     .use(express.urlencoded({ extended: true }))
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+        );
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        next();
+    })
     .use('/', require('./routes'));
 
 const db = require('./models');
