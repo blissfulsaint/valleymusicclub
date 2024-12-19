1ddb6c700d1c02a63d39ac62ed20f026c1879def
Brandon Lisonbee <blissfulsaint99@gmail.com>
2024-12-19 21:39:10 +0000

    Update CardLink component with prop for imgSrc and styles for bg-image

--- app/ui/components/CardLink/CardLink.tsx
+++ app/ui/components/CardLink/CardLink.tsx
@@ -15,13 +15,24 @@ export default function CardLink({
     width = 'w-full',
     height = 'h-64',
     bgColor = 'bg-slate-400',
+    imgSrc,
     href = '#',
     title
 }: CardLinkProps) {
     return (
         <div className={`${colspan}`}>
             <Link href={href}>
-                <div className={`${width} ${height} ${bgColor} rounded-xl overflow-hidden cursor-pointer relative`}>
+            <div
+                    className={`${width} ${height} ${bgColor} rounded-xl overflow-hidden cursor-pointer relative`}
+                >
+                    <div
+                        className="absolute inset-0 transition-transform transform scale-100 hover:scale-110"
+                        style={{
+                            backgroundImage: `url('${imgSrc}')`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                        }}
+                    ></div>
                     <p className="absolute bottom-0 left-0 w-full text-center p-2 py-3 text-xl bg-primaryColor">{title}</p>
                 </div>
             </Link>
--- app/ui/components/HomeDashboard/HomeDashboard.tsx
+++ app/ui/components/HomeDashboard/HomeDashboard.tsx
@@ -3,11 +3,11 @@ import CardLink from "../CardLink/CardLink";
 export default function HomeDashboard() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-3">
-            <CardLink colspan="md:col-span-2" href="#" title="Join Our Club" />
-            <CardLink href="#" title="About Us" />
-            <CardLink href="#" title="Events"/>
-            <CardLink href="#" title="Music Festivals"/>
-            <CardLink href="#" title="Competitions and Awards"/>
+            <CardLink colspan="md:col-span-2" href="#" title="Join Our Club" imgSrc="/studentgroup.jpeg" />
+            <CardLink href="#" title="About Us" imgSrc="/teachergroup1.jpeg" />
+            <CardLink href="#" title="Events" imgSrc="/teachergroup3.jpeg" />
+            <CardLink href="#" title="Music Festivals" imgSrc="/student1.jpeg" />
+            <CardLink href="#" title="Competitions and Awards" imgSrc="/teachergroup2.jpeg" />
         </div>
     )
 }
