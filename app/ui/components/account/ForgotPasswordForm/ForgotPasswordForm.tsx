4767ad92999306cc32c8a73ec52412227a0db5c5
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-02-07 05:52:51 +0000

    Create ForgotPasswordForm component

---
+++ app/ui/components/account/ForgotPasswordForm/ForgotPasswordForm.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { Separator } from "blisskit-ui";
+import Form from "../../vmc-form/Form/Form";
+import OutlineFieldset from "../../vmc-form/OutlineFieldset/OutlineFieldset";
+import OutlineFieldsetLegend from "../../vmc-form/OutlineFieldsetLegend/OutlineFieldsetLegend";
+import InputContainer from "../../vmc-form/InputContainer/InputContainer";
+import OutlineInput from "../../vmc-form/Input/Input";
+import FormButton from "../../vmc-form/FormButton/FormButton";
+import StatusMessage from "../../vmc-form/StatusMessage/StatusMessage";
+import PageLink from "../../PageLink/PageLink";
+
+export default function ForgotPasswordForm() {
+    return (
+        <>
+            <Separator size="sm" />
+            <Form action={''}>
+                <OutlineFieldset>
+                    <OutlineFieldsetLegend>Reset Password</OutlineFieldsetLegend>
+                </OutlineFieldset>
+            </Form>
+        </>
+    )
+}
