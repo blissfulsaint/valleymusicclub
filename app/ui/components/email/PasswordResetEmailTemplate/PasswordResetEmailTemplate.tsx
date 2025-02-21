4ca535d9d878bb624e11b138a66cbb36c7cf2f99
Brandon Lisonbee <blisonbee213596@gmail.com>
2025-02-20 19:34:23 -0700

    Revise password reset email template

--- app/ui/components/email/PasswordResetEmailTemplate/PasswordResetEmailTemplate.tsx
+++ app/ui/components/email/PasswordResetEmailTemplate/PasswordResetEmailTemplate.tsx
@@ -12,7 +12,6 @@ export default function PasswordResetEmailTemplate({
     return (
         <div>
             <h1>Hello, {firstName}!</h1>
-            <p><strong>***ATTENTION*** This is a testing email and the link below will take you to the home page of the current deployment of the Valley Music Club website which is still in active development.</strong></p>
             <p>We received a request to reset your password at <strong>ValleyMusicClub.com</strong>. Click the button below to reset your password:</p>
             <a href={resetLink} target='_blank'>Reset Password</a>
             <p>If you did not request this, you can safely ignore this email. No changes have been made to your account.</p>
