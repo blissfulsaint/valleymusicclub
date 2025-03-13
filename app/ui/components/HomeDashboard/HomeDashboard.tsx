75095accc1f4ca2f439ed260c637282fc5e72f36
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-03-13 18:53:17 +0000

    Make "Join Our Club" link on HomeDashboard dynamic and functional

--- app/ui/components/HomeDashboard/HomeDashboard.tsx
+++ app/ui/components/HomeDashboard/HomeDashboard.tsx
@@ -1,16 +1,31 @@
-import { CardLink } from "blisskit-ui"
+'use client';
+import { CardLink } from "blisskit-ui";
+import { useAuth } from "@/app/context/AuthContext";
 
 export default function HomeDashboard() {
+    const { isAuthenticated } = useAuth();
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-3">
-            <CardLink 
+            {isAuthenticated ? (
+                <CardLink 
                 textClassName="bg-primaryColor" 
                 colspan="md:col-span-2" 
-                href="/page-in-development" 
+                href="/account" 
+                title="View My Account" 
+                ariaLabel="View your account dashboard" 
+                imgSrc="/studentgroup.jpeg"
+            />
+            ) : (
+                <CardLink 
+                textClassName="bg-primaryColor" 
+                colspan="md:col-span-2" 
+                href="/create-account" 
                 title="Join Our Club" 
                 ariaLabel="Create an Account on ValleyMusicClub.com" 
                 imgSrc="/studentgroup.jpeg"
             />
+            )}
             <CardLink 
                 textClassName="bg-primaryColor" 
                 href="/page-in-development" 
