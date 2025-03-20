9a8ec08d011eb7556d8b60ef3f33cb2419cdfa68
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-03-20 18:44:34 +0000

    Add testing log

--- app/api/auth/validate-token/route.ts
+++ app/api/auth/validate-token/route.ts
@@ -11,6 +11,8 @@ export async function GET(req: Request) {
     const results = await sql`SELECT user_id, expires_at, used FROM public.password_recovery_token WHERE token = ${token}`;
     const tokenData = results.rows[0];
 
+    console.log(tokenData);
+
     if (!tokenData || tokenData.used || new Date() > new Date(tokenData.expires_at)) {
         return Response.json({ error: 'Invalid or expired token' }, { status: 400 });
     }
