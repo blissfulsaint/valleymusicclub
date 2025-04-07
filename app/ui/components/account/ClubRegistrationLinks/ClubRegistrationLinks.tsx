0a5d428196db6f70be845bb5911b48f14846da47
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-04-07 22:19:30 +0000

    Add async/await to ClubRegistrationLinks test code

--- app/ui/components/account/ClubRegistrationLinks/ClubRegistrationLinks.tsx
+++ app/ui/components/account/ClubRegistrationLinks/ClubRegistrationLinks.tsx
@@ -1,4 +1,4 @@
-export default function ClubRegistrationLinks() {
+export default async function ClubRegistrationLinks() {
     // Code to test skeleton component fallback
     const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
@@ -8,7 +8,7 @@ export default function ClubRegistrationLinks() {
     console.log('Done waiting!');
     }
 
-    doSomething();
+    await doSomething();
     // End of test code
     return (
         <p>This is the ClubRegistrationLinks component!</p>
