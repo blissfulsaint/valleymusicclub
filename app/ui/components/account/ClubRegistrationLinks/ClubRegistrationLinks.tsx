8a78ef763d481383fc760d74c1302f36368fb55b
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-04-07 22:33:07 +0000

    Update test code

--- app/ui/components/account/ClubRegistrationLinks/ClubRegistrationLinks.tsx
+++ app/ui/components/account/ClubRegistrationLinks/ClubRegistrationLinks.tsx
@@ -1,16 +1,7 @@
+import { getTestData } from "@/app/lib/utils/test";
+
 export default async function ClubRegistrationLinks() {
-    // Code to test skeleton component fallback
-    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-
-    async function doSomething() {
-    console.log('Waiting...');
-    await delay(2000); // Waits 2 seconds
-    console.log('Done waiting!');
-    }
-
-    await doSomething();
-    // End of test code
-    return (
-        <p>This is the ClubRegistrationLinks component!</p>
-    )
-}
+    const message = await getTestData();
+  
+    return <p>{message}</p>;
+  }
