7d9d3b452532785bff6b4c56a24fb890018d04cd
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-04-07 22:37:49 +0000

    Add Console.logs for debugging

--- app/ui/components/account/ClubRegistrationLinks/ClubRegistrationLinks.tsx
+++ app/ui/components/account/ClubRegistrationLinks/ClubRegistrationLinks.tsx
@@ -1,7 +1,9 @@
 import { getTestData } from "@/app/lib/utils/test";
 
 export default async function ClubRegistrationLinks() {
+    console.log("Getting test data...");
     const message = await getTestData();
+    console.log("Test data received: " + message);
   
     return <p>{message}</p>;
   }
