7ebf90d75555573948face8d11925ca1f1e10e98
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-04-07 22:16:52 +0000

    Comment TermContext component for testing

--- app/context/TermContext.tsx
+++ app/context/TermContext.tsx
@@ -1,8 +1,8 @@
-'use client';
-import { createContext, useContext, useState, useEffect } from 'react';
-import { Term } from '../lib/db/definitions';
+// 'use client';
+// import { createContext, useContext, useState, useEffect } from 'react';
+// import { Term } from '../lib/db/definitions';
 
-type TermContextType = {
-    term: Term | null;
-    loading: boolean;
-}
+// type TermContextType = {
+//     term: Term | null;
+//     loading: boolean;
+// }
