cb134b1cee14b0ed59cf9a64276658fcc0530083
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-05-14 19:31:49 +0000

    Update getCurrentTerm function, implement test code

--- app/lib/utils/term.ts
+++ app/lib/utils/term.ts
@@ -1,13 +1,18 @@
 import { sql } from "@vercel/postgres";
 import { Term } from "../db/definitions";
 
-export async function getCurrentTerm() {
+export async function getCurrentTerm(): Promise<Term | null> {
     try {
-        const term = await sql<Term>`SELECT * FROM public.term WHERE CURDATE() BETWEEN term_start_date AND term_end_date`
+        const result = await sql<Term>`SELECT * FROM public.term WHERE CURRENT_DATE BETWEEN term_start_date AND term_end_date`;
 
-        return term;
+        // Check if any rows were returned
+        if (result.rows.length > 0) {
+            return result.rows[0];
+        }
+
+        return null;
     } catch (error) {
-        console.error('Database Error: ', error);
+        console.error('Database Error:', error);
         return null;
     }
 }
--- app/ui/components/account/ClubRegistrationLinks/ClubRegistrationLinks.tsx
+++ app/ui/components/account/ClubRegistrationLinks/ClubRegistrationLinks.tsx
@@ -1,9 +1,11 @@
 import { getTestData } from "@/app/lib/utils/test";
+import { getCurrentTerm } from "@/app/lib/utils/term";
+import { Term } from "@/app/lib/db/definitions";
 
 export default async function ClubRegistrationLinks() {
-    console.log("Getting test data...");
-    const message = await getTestData();
-    console.log("Test data received: " + message);
+    console.log("Getting current term...");
+    const currentTerm: Term | null = await getCurrentTerm();
+    console.log("Term data received: " + currentTerm);
   
     return <p>In time, you will be able to access links to register and pay necessary dues to Valley Music Club right here!</p>;
 }
