192735f120c39046a410d60c1f6bac6704df6bde
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-05-14 19:44:11 +0000

    Implement ClientLogger component for testing

--- app/ui/components/account/ClubRegistrationLinks/ClubRegistrationLinks.tsx
+++ app/ui/components/account/ClubRegistrationLinks/ClubRegistrationLinks.tsx
@@ -1,10 +1,16 @@
 import { getCurrentTerm } from "@/app/lib/utils/term";
 import { Term } from "@/app/lib/db/definitions";
+import ClientLogger from "../../test/ClientLogger/ClientLogger";
 
 export default async function ClubRegistrationLinks() {
     console.log("Getting current term...");
     const currentTerm: Term | null = await getCurrentTerm();
     console.log("Term data received: " + currentTerm);
   
-    return <p>In time, you will be able to access links to register and pay necessary dues to Valley Music Club right here!</p>;
+    return (
+        <>
+            <ClientLogger term={currentTerm} />
+            <p>In time, you will be able to access links to register and pay necessary dues to Valley Music Club right here!</p>
+        </>
+    );
 }
