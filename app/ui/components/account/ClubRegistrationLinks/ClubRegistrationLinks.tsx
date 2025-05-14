17193f7a63194ed25a69cb77183fed7c237bfebd
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-05-14 19:50:19 +0000

    Dynamically display term for testing purposes

--- app/ui/components/account/ClubRegistrationLinks/ClubRegistrationLinks.tsx
+++ app/ui/components/account/ClubRegistrationLinks/ClubRegistrationLinks.tsx
@@ -11,6 +11,11 @@ export default async function ClubRegistrationLinks() {
         <>
             <ClientLogger term={currentTerm} />
             <p>In time, you will be able to access links to register and pay necessary dues to Valley Music Club right here!</p>
+            {currentTerm?.school_year ? (
+                <p>{currentTerm.school_year}</p>
+            ) : (
+                <p>No term found</p>
+            )}
         </>
     );
 }
