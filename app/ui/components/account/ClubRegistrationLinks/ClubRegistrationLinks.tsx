fe73f4b9e1fd37a26b537ac5a078bb1d68233096
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-05-14 22:33:53 +0000

    Create date format helper, consume in ClubRegistrationLinks

--- app/ui/components/account/ClubRegistrationLinks/ClubRegistrationLinks.tsx
+++ app/ui/components/account/ClubRegistrationLinks/ClubRegistrationLinks.tsx
@@ -1,6 +1,7 @@
 import { cookies } from 'next/headers';
 import { verify } from 'jsonwebtoken';
 import { getCurrentTerm } from '@/app/lib/utils/term';
+import { formatDate } from '@/app/lib/utils/helpers';
 import { clubDuesPaid } from '@/app/lib/utils/clubDues';
 import type { Term } from '@/app/lib/db/definitions';
 
@@ -40,7 +41,7 @@ export default async function ClubRegistrationLinks() {
                 <>
                     <p>Thank you for your interest in the Valley Music Club! We are glad you are here and look forward to providing a wide variety of musical and educational experiences for you and your students. We look forward to your participation as we work together to teach, encourage, support, and celebrate musical growth and the achievements of musicians in our community.</p>
                     <h3>Join Our Club Today!</h3>
-                    <p>{currentTerm?.school_year} Club Registration is due: {currentTerm?.club_dues_deadline}</p>
+                    <p>{currentTerm?.school_year} Club Registration is due: {currentTerm && formatDate(currentTerm.club_dues_deadline)}</p>
                     <p>Membership in VMC will give you and your students opportunities to participate in luncheons, workships, clinics, and the annual Festival with the National Federation of Music Clubs.</p>
                     <PageLink href='/page-in-development'>Join Our Club!</PageLink>
                     <p>Questions? Email <PageLink href='mailto:valleymusicclubaz@gmail.com'>valleymusicclubaz@gmail.com</PageLink>.</p>
