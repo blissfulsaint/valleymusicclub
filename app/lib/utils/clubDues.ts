90f697868789aa6a5dddf03080c51c94d7ca0ddb
Brandon Lisonbee <blisonbee213596@gmail.com>
2025-07-18 18:35:36 -0600

    Add necessary server actions to clubDues.ts

--- app/lib/utils/clubDues.ts
+++ app/lib/utils/clubDues.ts
@@ -1,3 +1,6 @@
+// Any functions relating to the club_dues table in the DB are included here.
+// This includes many-to-many tables such as user_club_dues
+
 import { sql } from "@vercel/postgres";
 import { ClubDues } from "../db/definitions";
 
@@ -25,4 +28,34 @@ export async function clubDuesPaid(user_id: string, term_id: string): Promise<Us
     `;
 
     return result.rows[0];
+}
+
+export async function addUserClubDues(user_id: string, club_dues_id: string): Promise<number | null> {
+    const result = await sql`
+        INSERT INTO user_club_dues(user_id, club_dues_id, paid)
+        VALUES (${user_id}, ${club_dues_id}, FALSE)
+    `;
+
+    return result.rowCount;
+}
+
+export async function setClubDuesTransactionDetailsByCompositeId(user_id: string, club_dues_id: string, paid: boolean, payment_mode: string, transaction_id: string): Promise<number | null> {
+    const result = await sql`
+        UPDATE user_club_dues
+        SET paid = ${paid}, payment_mode = ${payment_mode}, transaction_id = ${transaction_id}
+        WHERE club_dues_id = ${club_dues_id} AND user_id = ${user_id}
+    `;
+
+    return result.rowCount;
+}
+
+export async function checkExistingClubDuesByCompositeId(user_id: string, club_dues_id: string): Promise<boolean> {
+    const result = await sql`
+        SELECT 1
+        FROM user_club_dues
+        WHERE club_dues_id = ${club_dues_id} AND user_id = ${user_id}
+        LIMIT 1
+    `;
+
+    return result.rows.length > 0;
 }
